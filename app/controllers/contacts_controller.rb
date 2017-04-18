c2b2df849c07690ec407c709483ad36ddcc63b7e
Andy Le <andy.le007@yahoo.com>
2017-04-18 22:53:12 +0000

    Added create action to save to the database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,22 @@
 class ContactsController < ApplicationController
    def new
+      #instant variable if it has a @ symbol
+      @contact = Contact.new
    end
-    
+   
+   def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         redirect_to new_contact_path, notice: "Comment sent!"
+      else
+         redirect_to new_contact_path, notice: "Error occured, please check that all forms are filled"
+      end
+   end
+   
+   #security feature
+   private
+      def contact_params
+         params.require(:contact).permit(:name, :email, :comments)
+      end
+      
 end
