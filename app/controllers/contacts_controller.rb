3d46e5e4095536370f9262ceb72a04e538f3ceb9
Andy Le <andy.le007@yahoo.com>
2017-04-25 16:36:51 +0000

    Trigger the mailer from the controller file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,6 +10,17 @@ class ContactsController < ApplicationController
    def create
       @contact = Contact.new(contact_params) #[error, error2] the error will be in an array
       if @contact.save
+         #lift things from form field
+         #this is saying "hey go into the contact form after it has been saved and
+         #grab the name the customer put in, and the email and the comments"
+         #and store them in the 'name', 'email', and 'body' variable and use those
+         #variables on line 22 when we deliver that email or message.
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:comments]
+         #This sends the email to us, to andy.le007@yahoo.com
+         ContactMailer.contact_email(name, email, body).deliver
+         
          flash[:success] = "Message sent."
          redirect_to new_contact_path
       else
